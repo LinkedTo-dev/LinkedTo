975f5bb8b8b16032d6a03e2d1ed0910c93d35f38
enzymii <enzymiix@gmail.com>
2021-12-23 00:39:25 +0800

    feat(src/components/map.tsx): add fetch data for Map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,6 +5,7 @@ import * as echarts from 'echarts/core';
 import { TitleComponent, TooltipComponent } from 'echarts/components';
 import { ScatterChart, EffectScatterChart } from 'echarts/charts';
 import { CanvasRenderer } from 'echarts/renderers';
+import { fetchData } from '../utils/fetch';
 
 echarts.use([
   TitleComponent,
@@ -14,87 +15,36 @@ echarts.use([
   CanvasRenderer,
 ]);
 
-function setCityData() {
-  return Math.round(Math.random() * 100);
+interface MapProps {
+  name: string;
+  value: number;
 }
-let option;
-const data = [
-  // { name: '贵州', value: setCityData() },
-  // { name: '广东', value: setCityData() },
-  // { name: '河南', value: setCityData() },
-  // { name: '云南', value: setCityData() },
-  // { name: '北京', value: setCityData() },
-  // { name: '天津', value: setCityData() },
-  // { name: '上海', value: setCityData() },
-  // { name: '重庆', value: setCityData() },
-  // { name: '海南', value: setCityData() },
-  // { name: '台湾', value: setCityData() },
-  // { name: '香港', value: setCityData() },
-  // { name: '澳门', value: setCityData() },
-  // { name: '陕西', value: setCityData() },
-  // { name: '吉林', value: setCityData() },
-  // { name: '福建', value: setCityData() },
-  // { name: '江苏', value: setCityData() },
-  // { name: '浙江', value: setCityData() },
-  // { name: '江西', value: setCityData() },
-  // { name: '湖北', value: setCityData() },
-  // { name: '辽宁', value: setCityData() },
-  // { name: '黑龙江', value: setCityData() },
-  // { name: '湖南', value: setCityData() },
-  // { name: '安徽', value: setCityData() },
-  // { name: '山东', value: setCityData() },
-  // { name: '新疆', value: setCityData() },
-  // { name: '广西', value: setCityData() },
-  // { name: '甘肃', value: setCityData() },
-  // { name: '河北', value: setCityData() },
-  // { name: '山西', value: setCityData() },
-  // { name: '青海', value: setCityData() },
-  // { name: '西藏', value: setCityData() },
-  // { name: '四川', value: setCityData() },
-  // { name: '宁夏', value: setCityData() },
-  // { name: '内蒙古', value: setCityData() },
-];
-const ChinaMap = (props) => {
+
+const ChinaMap = () => {
   const type = useContext(typeContext);
 
-  useEffect(() => {
-    const myMain = document.getElementById('anlc');
-    const myChart = echarts.init(myMain);
-    option = {
-      title: {
-        text: '中国地图',
-        left: 'center',
-      },
-      tooltip: {
-        trigger: 'item',
-      },
+  const [data, setData] = useState<MapProps[]>([]);
+
+  const drawMap = () => {
+    const mapMain = document.getElementById('anlc');
+    const mapChart = echarts.init(mapMain);
+    const option = {
+      title: { text: '产业地图', left: 'center' },
+      tooltip: { trigger: 'item' },
       visualMap: {
         show: true,
         x: 'left',
         y: 'bottom',
-        splitList: [
-          { start: 90, end: 100 },
-          { start: 80, end: 90 },
-          { start: 70, end: 80 },
-          { start: 60, end: 70 },
-          { start: 50, end: 60 },
-          { start: 40, end: 50 },
-          { start: 30, end: 40 },
-          { start: 20, end: 30 },
-          { start: 10, end: 20 },
-          { start: 0, end: 10 },
-        ],
+        splitList: [{ start: 0, end: 100 }],
         color: [
-          '#f47920',
-          '#444693',
-          '#224b8f',
-          '#102b6a',
-          '#494e8f',
-          '#ae6642',
-          '#6f60aa',
-          '#694d9f',
-          '#8552a1',
-          '#401c44',
+          '#f7fcfd',
+          '#e0ecf4',
+          '#bfd3e6',
+          '#9ebcda',
+          '#8c96c6',
+          '#8c6bb1',
+          '#88419d',
+          '#810f7c',
         ],
       },
       series: [
@@ -103,19 +53,23 @@ const ChinaMap = (props) => {
           type: 'map',
           mapType: 'china',
           roam: true,
-          label: {
-            normal: {
-              show: true,
-            },
-            emphasis: {
-              show: false,
-            },
-          },
+          label: { normal: { show: true }, emphasis: { show: false } },
           data: data,
         },
       ],
     };
-    option && myChart.setOption(option);
+    option && mapChart.setOption(option);
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setData(await fetchData('/map', { type }));
+      } catch (e) {
+        //TODO: handle exception
+      }
+    })();
+    drawMap();
   }, []);
   return <div id='anlc' style={{ height: '50vh', width: '90%' }}></div>;
 };
